55ad7a93f50f1c0f97681a108304d535a84830c3
takahiro nakano <shigurelogic@gmail.com>
2016-06-17 07:27:16 +0000

    a

---
+++ config/initializers/messenger_bot.rb
@@ -0,0 +1,5 @@
+Messenger::Bot.config do |config|
+  config.access_token = "EAAYzXltTDCABAI2V3i6P6TKo9DCTKcYZBWiMbQqjLgu0AGHMcjZCE6SGpXeTqvcSYzo6zKZBPLC4UwP5Cdpf44teZBLCoKVkUJf11VpPgg6X2tp2ZABOAlK0vqfexFOi2aU23VNeZA72AbqThVIR0ZAHblGzoXyqIrFcRWFj6ZCTMQZDZD"
+  config.validation_token = "hoge"
+  config.secret_token = "4f03aa1ddce83fde566e086c6de7b8b3"
+end
