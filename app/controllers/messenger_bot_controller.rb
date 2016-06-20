c3899b9c3f7ffc1dcad43ee398f99e5887385033
takahiro nakano <shigurelogic@gmail.com>
2016-06-20 06:08:33 +0000

    aa

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,20 +1,52 @@
 class MessengerBotController < ActionController::Base
-  def message(event, sender)
+  #def message(event, sender)
     # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
      #sender.reply({ text: "#{event['message']['text']}" })
    
     #text.sender = event['message']['text']
     
 
-    sender.reply({ text: "gameをしませんか？" })
+    #sender.reply({ text: "gameをしませんか？" })
 
       
-  end
+  #end
+    profile = sender.get_profile[:body]
+    profile_last_name = profile['last_name']
+    profile_first_name = profile['first_name']
+ 
+    sender.reply({ "attachment":{
+            "type":"template",
+            "payload":{
+                "template_type":"button",
+                "text":"#{profile_last_name} #{profile_first_name}さんこんにちは",
+                "buttons":[
+                    {
+                        "type":"postback",
+                        "title":"OVER",
+                        "payload":"OVER"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"UNDER",
+                        "payload":"UNDER"
+                    }
+                ]
+            }
+         }
+      })
+      
+      
+def postback(event, sender)
+    payload = event["postback"]["payload"]
+    case payload
+    when "OVER"
+      sender.reply({ text: "上が押されたよ！" })
+    when "UNDER"
+      sender.reply({ text: "下が押されたよ！" })
+    end
+end
   
   
-     # sender.reply(
-     # { text: "#{event['message']['text']}" })
-
   def delivery(event, sender)
   end
 
@@ -26,4 +58,3 @@ class MessengerBotController < ActionController::Base
     end
   end
 end
-
