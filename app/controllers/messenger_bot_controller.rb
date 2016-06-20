6597b66ebdfce7683a9238a09b52f201fa92563d
takahiro nakano <shigurelogic@gmail.com>
2016-06-20 03:37:02 +0000

    a

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,15 +1,15 @@
 class MessengerBotController < ActionController::Base
   def message(event, sender)
     # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
-     sender.reply({ text: "#{event['message']['text']}" })
+     #sender.reply({ text: "#{event['message']['text']}" })
    
     #text.sender = {text: "#{event['message']['text']}"}
     
-   # if text.sender.nil?
-    #  sender.reply({ text: "#{gameをしませんか？}" })
-    #else
-     # sender.reply({ text: "#{もういいです！！}"})
-    #end
+   if text.sender != null
+    sender.reply({ text: "gameをしませんか？" })
+    else
+    sender.reply({ text: "もういいです！！"})
+   end
       
   end
   
