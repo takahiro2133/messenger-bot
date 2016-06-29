d8d141e6317cb0d52fad2bcd94cdc16a85290ec7
takahiro nakano <shigurelogic@gmail.com>
2016-06-29 01:11:45 +0000

    a

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -39,6 +39,40 @@ class MessengerBotController < ActionController::Base
             
                     sender.reply({ text: "#{city}の天気はこんな感じです。" })
                     
+                    sender.reply({ "attachment": {
+                                    "type":"template",
+                                    "payload":{
+                                        "template_type":"generic",
+                                        "elements":[
+                                            {
+                                                "title":"#{today}",
+                                                "image_url":"#{weather_today['image']['url']}",
+                                                "subtitle":"#{max}\n#{min}",
+                                                "buttons":[
+                                                    {
+                                                        "type":"web_url",
+                                                        "url":"#{link}",
+                                                        "title":"詳細を見る"
+                                                    }
+                                                          ]
+                                            },{
+                                                "title":"#{tomorrow}",
+                                                "image_url":"#{weather_tomorrow['image']['url']}",
+                                                "subtitle":"#{max_t}\n#{min_t}",
+                                                "buttons":[
+                                                    {
+                                                        "type":"web_url",
+                                                        "url":"#{link}",
+                                                        "title":"詳細を見る"
+                                                    }
+                                                          ]
+                                            }
+                                                    ]
+                                      
+                                              }
+                                                            }
+                                             })
+                    
             elsif text == "こんにちは"
                sender.reply(text: "hello")
                
