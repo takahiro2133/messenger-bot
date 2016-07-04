c0f4d091811f7fba84727d2693f07787d35e9a13
takahiro nakano <shigurelogic@gmail.com>
2016-07-04 15:27:40 +0000

    b

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,24 +1,11 @@
 class MessengerBotController < ActionController::Base
   
     def message(event,sender)
-　    　sender.reply(text:"hello")
+　    　sender.reply(text: "hello")
     end
   
  
     def delivery(event, sender)
-    end     
-  
-    def postback(event, sender)
-        payload = event["postback"]["payload"]
-      case payload
-      when "using_c"
-        sender.reply(text: "C！")
-      when "other_c"
-        sender.reply(text: "otherC")
-      when "music_c"
-        sender.reply(text:text_C_music)
-
-      end
     end
   
 end 
