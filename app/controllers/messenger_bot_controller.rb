514d0ac727fb047e19f6cfa7a12b42ef23cada4d
takahiro nakano <shigurelogic@gmail.com>
2016-07-04 17:31:08 +0000

    b

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -9,7 +9,6 @@ class MessengerBotController < ActionController::Base
 #コード
       text_C  = 'http://mfc-music.com/wp-content/uploads/2015/03/C%E3%82%B3%E3%83%BC%E3%83%89.png'
       text_G  = 'http://mfc-music.com/wp-content/uploads/2015/03/G%E3%82%B3%E3%83%BC%E3%83%89.png'
-      text_A = 'https://www.dropbox.com/s/ftzz61c7wvuoxw7/a3-s.png?dl=0'
       text_Am = 'http://mfc-music.com/wp-content/uploads/2015/03/Am%E3%82%B3%E3%83%BC%E3%83%89.png'
 
     
@@ -22,7 +21,7 @@ class MessengerBotController < ActionController::Base
         code[t] = [code_shinkou[t],code_shinkou_music[t]]
       }
     
-      recommend =Array.new
+      recommend = Array.new
         0.upto(6){|t|
         recommend[t] = [music_recommend[t],music_recommend_tab[t],music_recommend_oto[t]]
       }
@@ -30,7 +29,7 @@ class MessengerBotController < ActionController::Base
 
       text_random = Array["ギターって...いいよな", "ギター練習したのか？"]
       music_recommend = Array["Layla(Eric Clapton)", "Are You Gonna Go My Way（Lenny Kravitz)", "Walk This Way（Aerosmith)","20th Century Boy（T.Rex)", "Whole Lotta Love（Led Zeppelin）", "Helter Skelter（The Beatles）","Painkiller（Judas Priest）","歌舞伎町の女王（椎名林檎）"]
-      music_recommend_tab = Array["https://www.dropbox.com/home?preview=SP32-20041117-174117.gif", "http://bandbigginer.boo.jp/wp-content/uploads/20120213areyougonnagomyway.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/20120213walkthisway.jpg","http://bandbigginer.boo.jp/wp-content/uploads/2012021320thcenturyboy.jpg","http://bandbigginer.boo.jp/wp-content/uploads/20120213wholelottalove.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/20120213helterskelter.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/20120213painkiller.jpg", "歌舞伎町の女王（椎名林檎）"]
+      music_recommend_tab = Array["https://www.dropbox.com/home?preview=SP32-20041117-174117.gif", "http://bandbigginer.boo.jp/wp-content/uploads/20120213areyougonnagomyway.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/20120213walkthisway.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/2012021320thcenturyboy.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/20120213wholelottalove.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/20120213helterskelter.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/20120213painkiller.jpg", "歌舞伎町の女王（椎名林檎）"]
       music_recommend_oto = Array["A","B","C","D","E","F","G"]
       code_minor = Array["Am", "Bm", "Cm", "Dm", "Em", "Fm", "Gm"]
       code_major = Array["A", "B", "C", "D", "E", "F", "G"]
@@ -43,7 +42,7 @@ class MessengerBotController < ActionController::Base
       code_shinkou_minor_music = Array[]
       flase_recommend = Array[]
 #天気　１０通りくらい
-      music_recommend_hare = Array["Back in Black(AC/DC)","Brainstorm(Arctic Monkeys)","Loney Boy(The Loney Boy)","Smells Like Tenn Spirit(Nirvana)","Voodoo Child (Jimi Hendrix)","Another number(The Cribes)","Take The Long Road And Walk It(The Music)","Immigrant Song(Jimi Hendrix)","12:51(The Strokes)","Supersonic(Oasis)","Can't Stop(Red Hot Chili Peppers)"]
+      music_recommend_hare = Array["Back in Black(AC/DC)", "Brainstorm(Arctic Monkeys)", "Loney Boy(The Loney Boy)", "Smells Like Tenn Spirit(Nirvana)", "Voodoo Child (Jimi Hendrix)", "Another number(The Cribes)", "Take The Long Road And Walk It(The Music)", "Immigrant Song(Jimi Hendrix)", "12:51(The Strokes)", "Supersonic(Oasis)", "Can't Stop(Red Hot Chili Peppers)"]
       music_recommend_ame = Array[]
       music_recommend_kumori = Array[]
 
@@ -80,174 +79,8 @@ class MessengerBotController < ActionController::Base
                       　　sender.reply({ text: "#{today}"})
                     else  sender.reply( text: "天気なんか知るか" )
                     end
-                    
-                    
-                    
-            elsif text.include?("フレーズ") & text.include?("おすすめ") or text.include?("リフ")
-                recommend_sample = recommend.sample
-                sender.reply(text: "[曲名]\n#{recommend_sample[0]}\n[サンプル音]\n#{recommend_sample[2]}\n[TAB]")
-                sender.reply({ "attachment": {"type": "image","payload": {"url": recommend_sample[1]}}})
-                
-            elsif text.include?("コード進行")
-                code_sample = code.sample
-                sender.reply(text: "#{code_sample[0]}\n[サンプル音]\n#{code_sample[1]}")
-                
-            elsif text.include?("曲") & text.include?("おすすめ")
-                sender.reply(text: "#{music_recommend.sample}")
-            
- #奏法などに対する応答
-            elsif text.include?("アルペジオ") && text.end_with?("？") or text == ("アルペジオ")
-              sender.reply(text: "アルペジオは、1本1本の弦をバラバラに弾く奏法のことだな！")
-              
-              sender.reply({ "attachment": {
-              "type": "audio",
-              "payload": {
-                 "url":text_C_music
-                          }
-                                              }
-                           })
-            elsif text.include?("チョーキング") && text.end_with?("？") or text == ("チョーキング")
-               sender.reply(text: "チョーキングは、弦を弾いた後、押し弦を動かして音の高さを変える奏法のことだな！")
-              # sender.reply({ "attachment": {
-              #  "type": "image",
-              #  "payload": {
-              #    "url": "http://mfc-music.com/wp-content/uploads/2015/03/C%E3%82%B3%E3%83%BC%E3%83%89.png"
-              #            }
-              #                                }
-              #              })
-            
-            
-            #気分などに対する応答                
-            elsif text.include?("疲れた") or text.include?("しんどい") or text.include?("きつい")
-               sender.reply(text: "今の君は#{code_minor.sample}な気分なんだな！！そんな君にはこのフレーズを伝授しよう")
-               sender.reply(text: "#{code_shinkou.sample}")
-               
-                        #コードに対する応答
-           # elsif text.include?("コード一覧")
-            #    sender.reply({ "attachment": {"type": "image","payload": {"url": }}})
-            elsif text == "Amコード" or text == "Amコード" or text == "Am" or text == "Am"
-                sender.reply({ "attachment":{
-                 "type":"template",
-                         "payload":{
-                             "template_type":"generic",
-                             "elements":[
-                               {  "title":"Amコードの押さえ方",
-                                  "image_url":text_Am,
-                                  "buttons":[
-                                       {
-                                           "type":"postback",
-                                           "title":"Amコードを使ったコード進行",
-                                           "payload":"using_Am"
-                                       },
-                                       {
-                                           "type":"postback",
-                                           "title":"他のマイナーコード",
-                                           "payload":"other_Am"
-                                       },
-                                       {
-                                           "type":"postback",
-                                           "title":"Amコードのサンプル音",
-                                           "payload":"music_Am"
-                                       }
-                                            ]
-                              }
-                                ]       
-                                   }
-                                 }
-                             })
-            elsif text == "Aコード"  or text == "A" 
-                sender.reply({ "attachment":{
-                 "type":"template",
-                         "payload":{
-                             "template_type":"generic",
-                             "elements":[
-                               {  "title":"Aコードの押さえ方",
-                                  "image_url":text_A,
-                                  "buttons":[
-                                       {
-                                           "type":"postback",
-                                           "title":"Aコードを使ったコード進行",
-                                           "payload":"using_A"
-                                       },
-                                       {
-                                           "type":"postback",
-                                           "title":"他のマイナーコード",
-                                           "payload":"other_A"
-                                       },
-                                       {
-                                           "type":"postback",
-                                           "title":"Aコードのサンプル音",
-                                           "payload":"music_A"
-                                       }
-                                            ]
-                              }
-                                ]       
-                                   }
-                                 }
-                             })
-            elsif text == "Cコード" or text == "cコード" or text == "C" or text == "c"
-               sender.reply({ "attachment":{
-                 "type":"template",
-                         "payload":{
-                             "template_type":"generic",
-                             "elements":[
-                               {  "title":"Cコードの押さえ方",
-                                  "image_url":text_C,
-                                  "buttons":[
-                                       {
-                                           "type":"postback",
-                                           "title":"Cコードを使ったコード進行",
-                                           "payload":"using_c"
-                                       },
-                                       {
-                                           "type":"postback",
-                                           "title":"Cの関連コード",
-                                           "payload":"other_c"
-                                       },
-                                       {
-                                           "type":"postback",
-                                           "title":"Cコードのサンプル音",
-                                           "payload":"music_c"
-                                       }
-                                            ]
-                              }
-                                ]       
-                                   }
-                                 }
-                             })
-                             
-            elsif text == "Gコード" or text == "gコード" or  text == "G" or text == "g"
-               sender.reply({ "attachment":{
-                 "type":"template",
-                         "payload":{
-                             "template_type":"generic",
-                             "elements":[
-                               {  "title":"Gコードの押さえ方",
-                                  "image_url":text_G,
-                                  "buttons":[
-                                       {
-                                           "type":"postback",
-                                           "title":"Gコードを使ったコード進行",
-                                           "payload":"using_g"
-                                       },
-                                       {
-                                           "type":"postback",
-                                           "title":"Gの関連コード",
-                                           "payload":"other_g"
-                                       },
-                                       {
-                                           "type":"postback",
-                                           "title":"Gコードのサンプル音",
-                                           "payload":"music_g"
-                                       }
-                                            ]
-                              }
-                                ]       
-                                   }
-                                 }
-                             })
 
-            else sender.reply(text: "hello")
+          　else sender.reply(text: "hello")
             end
   end
   
