ae35349ca2d8daf448e7d43462860cc0f212f4a4
takahiro nakano <shigurelogic@gmail.com>
2016-07-04 15:17:15 +0000

    a

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,26 +1,24 @@
 class MessengerBotController < ActionController::Base
-    #require 'json'
-    #require 'open-uri'
-    #require 'uri'
-  
-    #BingAPIKEY = 'RWVDsTmHZY47DRyQQq4iJmvb6Ut1ENBPeRk+OdfFthY'
-  def message(event,sender)
-　　sender.reply(text: "hello")
-  end
+    def message(event,sender)
+　    　sender.reply(text: "hello")
+    end
   
  
-  def delivery(event, sender)
-  end     
+    def delivery(event, sender)
+    end     
   
-  def postback(event, sender)
+    def postback(event, sender)
         payload = event["postback"]["payload"]
-    case payload
-    when "using_c"
-      sender.reply(text: "C！")
-    when "other_c"
-      sender.reply(text: "otherC")
+      case payload
+      when "using_c"
+        sender.reply(text: "C！")
+      when "other_c"
+        sender.reply(text: "otherC")
+      when "music_c"
+        sender.reply(text:text_C_music)
+
+      end
     end
-  end
   
 end 
 
