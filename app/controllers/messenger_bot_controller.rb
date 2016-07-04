f17c32b303fc7993a1e781926a35bd3bf8aefec1
takahiro nakano <shigurelogic@gmail.com>
2016-07-04 09:58:54 +0000

    a

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -6,9 +6,81 @@ class MessengerBotController < ActionController::Base
     BingAPIKEY = 'RWVDsTmHZY47DRyQQq4iJmvb6Ut1ENBPeRk+OdfFthY'
   
   def message(event,sender)
-    sender_id = event['sender']['id']
     text = event['message']['text']
-    sender.reply(text: "hello")
+        #コード
+    text_C  = 'http://mfc-music.com/wp-content/uploads/2015/03/C%E3%82%B3%E3%83%BC%E3%83%89.png'
+    text_G  = 'http://mfc-music.com/wp-content/uploads/2015/03/G%E3%82%B3%E3%83%BC%E3%83%89.png'
+    text_F = ''
+    text_A = ''
+    text_B = ''
+    text_E = ''
+    text_D = ''
+    text_Fsm = ''
+    text_Am = 'http://mfc-music.com/wp-content/uploads/2015/03/Am%E3%82%B3%E3%83%BC%E3%83%89.png'
+    text_Em = ''
+    text_Bm = ''
+    text_code_all = ''
+    
+    #コード音
+    text_C_music = 'https://www.dropbox.com/home?preview=C%E3%82%B3%E3%83%BC%E3%83%89.mp3'
+   
+   
+    text_random = Array["ギターって...いいよな", "ギター練習したのか？"]
+    music_recommend = Array["Layla(Eric Clapton)", "Are You Gonna Go My Way（Lenny Kravitz)", "Walk This Way（Aerosmith)","20th Century Boy（T.Rex)", "Whole Lotta Love（Led Zeppelin）", "Helter Skelter（The Beatles）","Painkiller（Judas Priest）","歌舞伎町の女王（椎名林檎）"]
+    music_recommend_tab = Array["https://www.dropbox.com/home?preview=SP32-20041117-174117.gif", "http://bandbigginer.boo.jp/wp-content/uploads/20120213areyougonnagomyway.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/20120213walkthisway.jpg","http://bandbigginer.boo.jp/wp-content/uploads/2012021320thcenturyboy.jpg","http://bandbigginer.boo.jp/wp-content/uploads/20120213wholelottalove.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/20120213helterskelter.jpg", "http://bandbigginer.boo.jp/wp-content/uploads/20120213painkiller.jpg", "歌舞伎町の女王（椎名林檎）"]
+    music_recommend_oto = Array["A","B","C","D","E","F","G"]
+    code_minor = Array["Am", "Bm", "Cm", "Dm", "Em", "Fm", "Gm"]
+    code_major = Array["A", "B", "C", "D", "E", "F", "G"]
+    code_shinkou = Array["D - A - Bm - F#m - G - D - G - A", "F - G - Em - Am", "Am - F - G - C"]
+    code_shinkou_music = Array["aaa","bbb","ccc"]
+    flase_recommend = Array[]
+    
+    code = Array.new
+    0.upto(6){|t|
+      code[t] = [code_shinkou[t],code_shinkou_music[t]]
+    }
+    
+    recommend =Array.new
+      0.upto(6){|t|
+      recommend[t] = [music_recommend[t],music_recommend_tab[t],music_recommend_oto[t]]
+    }
+    
+          
+            if text.end_with?("天気")
+                    uri = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
+                    res = JSON.load(open(uri).read)
+                    weather_today = res['forecasts'][0]
+                    weather_tomorrow = res['forecasts'][1]
+                    link = res['link']
+                    city = res['location']['city']
+                    weather = weather_today['telop']
+            
+                    if weather_today['temperature']['max'] != nil
+                        max = "最高気温：#{weather_today['temperature']['max']['celsius']}℃"
+                    end
+                    if weather_today['temperature']['min'] != nil
+                        min = "最低気温：#{weather_today['temperature']['min']['celsius']}℃"
+                    end
+                    
+                    today = "#{weather_today['dateLabel']}の#{city}の天気は「#{weather_today['telop']}」だな。"
+                    
+                    if weather == '晴れ'
+                          sender.reply({ text: "#{today}今日におすすめの曲はこれだ！！" })
+                    elsif weather == '曇のち雨'
+                          sender.reply({ text: "#{today}こんなしみったれた日はこの曲でも聞いてロックしろ！！" })
+                    elsif weather == '雨'
+                          sender.reply({ text: "#{today}こんな日はこの曲でも聞いてロックしろ！！" })
+                    elsif weather == '曇り'
+                          recommend_sample = recommend.sample
+                          sender.reply({ text: "#{today}こんな日は#{recommend_sample[0]}でも聞いてロックしろ！！" })
+                    elsif weather == '曇時々雨'
+                          sender.reply({ text: "#{today}こんな日はこの曲でも聞いてロックしろ！！" })
+                 　 elsif weather == '曇時々晴'
+                      　　sender.reply({ text: "#{today}"})
+                    else  sender.reply( text: "天気なんか知るか" )
+                    end
+            else  sender.reply(text: "hello")
+            end
       
   end
   
