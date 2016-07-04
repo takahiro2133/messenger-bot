16f363b0491cf64b05c114faab92959ae13e3425
takahiro nakano <shigurelogic@gmail.com>
2016-07-04 14:55:27 +0000

    a

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -15,45 +15,6 @@ class MessengerBotController < ActionController::Base
   end     
   
   
-  def postback(event, sender)
-    text_C  = 'http://mfc-music.com/wp-content/uploads/2015/03/C%E3%82%B3%E3%83%BC%E3%83%89.png'
-    text_G  = 'http://mfc-music.com/wp-content/uploads/2015/03/G%E3%82%B3%E3%83%BC%E3%83%89.png'
-    text_Am = 'http://mfc-music.com/wp-content/uploads/2015/03/Am%E3%82%B3%E3%83%BC%E3%83%89.png'
-    
-    
-    text_C_music = 'https://www.dropbox.com/home?preview=C%E3%82%B3%E3%83%BC%E3%83%89.mp3'
-    
-    payload = event["postback"]["payload"]
-    case payload
-    when "using_c"
-      sender.reply(text: "C！")
-    when "other_c"
-      sender.reply(text: "otherC")
-    when "music_c"
-      sender.reply(text:text_C_music)
-      
-    when "using_A"
-      sender.reply(text: "A！")
-    when "other_A"
-      sender.reply(text: "otherA")
-    when "music_A"
-      sender.reply(text: "aaa")
-      
-    when "using_g"
-      sender.reply(text: "G！")
-    when "other_g"
-      sender.reply(text: "otherG")
-    when "music_g"
-      sender.reply(text: "はは")
-      
-    when "using_Am"
-      sender.reply(text: "Am！")
-    when "other_Am"
-      sender.reply(text: "otherAm")
-    when "music_Am"
-      sender.reply(text: "Am")
-    end
-  end
   
 end 
 
