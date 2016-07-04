b706cb0da5310987e0bf2b0294352da1ddc24b53
takahiro nakano <shigurelogic@gmail.com>
2016-07-04 18:05:18 +0000

    b

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -5,22 +5,12 @@ class MessengerBotController < ActionController::Base
   
   def message(event,sender)
       text = event['message']['text']
-           if text.end_with?("天気")
+           if text == "天気"
                     uri = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
                     res = JSON.load(open(uri).read)
                     weather_today = res['forecasts'][0]
-                    weather_tomorrow = res['forecasts'][1]
-                    link = res['link']
                     city = res['location']['city']
                     weather = weather_today['telop']
-            
-                    if weather_today['temperature']['max'] != nil
-                        max = "最高気温：#{weather_today['temperature']['max']['celsius']}℃"
-                    end
-                    if weather_today['temperature']['min'] != nil
-                        min = "最低気温：#{weather_today['temperature']['min']['celsius']}℃"
-                    end
-                    
                     today = "#{weather_today['dateLabel']}の#{city}の天気は「#{weather_today['telop']}」だな。"
                     
                     if weather == "晴れ"
@@ -30,13 +20,13 @@ class MessengerBotController < ActionController::Base
                     elsif weather == "雨"
                           sender.reply({ text: "#{today}こんな日はこの曲でも聞いてロックしろ！！" })
                     elsif weather == "曇り"
-                          recommend_sample = recommend.sample
-                          sender.reply({ text: "#{today}こんな日は#{recommend_sample[0]}でも聞いてロックしろ！！" })
+                          sender.reply({ text: "#{today}こんな日はでも聞いてロックしろ！！" })
                     elsif weather == "曇時々雨"
                           sender.reply({ text: "#{today}こんな日はこの曲でも聞いてロックしろ！！" })
-                 　 elsif weather == "曇時々晴"
+                 　  elsif weather == "曇時々晴"
                       　　sender.reply({ text: "#{today}"})
-                    else  sender.reply( text: "天気なんか知るか" )
+                    else  
+                      sender.reply( text: "天気なんか知るか" )
                     end
 #奏法などに対する応答
             elsif text.include?("アルペジオ") && text.end_with?("？") or text == ("アルペジオ")
