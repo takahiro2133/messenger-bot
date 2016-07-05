f3be25d66be03fcf39eef96bdf3ac4b89db8bfe7
takahiro nakano <shigurelogic@gmail.com>
2016-07-05 23:31:00 +0000

    a

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -15,7 +15,7 @@ class MessengerBotController < ActionController::Base
     code_shinkou_music = Array["aaa","bbb","ccc"]
 
     code_shinkou_major = Array["A-Fm-D-E","C-D-Em-G","C-G-Am-Em-F-C-F-G","Fm-A-B-E","Cadd9-G-D-Em7"]
-    code_shinkou_major_music = Array["http://kusapan.com/fbmbot/AFmDE.mp3","http://kusapan.com/fbmbot/CDEmG.mp3","http://kusapan.com/fbmbot/CGAmEmFCFG.mp3","http://kusapan.com/fbmbot/FmABE.mp3","http://kusapan.com/fbmbot/talar.mp3"]
+    code_shinkou_major_music = Array["http://kusapan.com/fbmbot/AFmDE.mp3","http://kusapan.com/fbmbot/CDEmG.mp3","http://kusapan.com/fbmbot/CGAmEFCFG.mp3","http://kusapan.com/fbmbot/FmABE.mp3","http://kusapan.com/fbmbot/talar.mp3"]
     code_shinkou_minor = Array["Am-Em-F-C","Am-Dm-E7-Am","Am-G-F-Em-Dm-C-Bm7-E7","D-A-Bm-Em-Fm-G-D-G-A","A-E-F#m7-E-DM7-C#m7-Bm7-E"]
     code_shinkou_minor_music = Array["http://kusapan.com/fbmbot/AmEmFC.mp3","http://kusapan.com/fbmbot/AmDmE7Am.mp3","http://kusapan.com/fbmbot/AmGFEmDmC.mp3","http://kusapan.com/fbmbot/DABmEmFmGDGA.mp3","http://kusapan.com/fbmbot/yamashita.mp3"]
 
